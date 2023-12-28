367529ba6be5f2a8e80b526bc32e3faab1b039ef
Exidex <16986685+Exidex@users.noreply.github.com>
2023-12-28 13:53:00 +0100

    Black-Golden theme

--- rust/client/src/ui/theme.rs
+++ rust/client/src/ui/theme.rs
@@ -17,11 +17,11 @@ impl GauntletTheme {
     pub fn new() -> Self {
         Self {
             theme: Theme::custom(Palette {
-                background: iced::color!(0x2A373E),
-                text: iced::color!(0xCFE7DF),
-                primary: iced::color!(0x77BCBF),
-                success: iced::color!(0x659B5E),
-                danger: iced::color!(0x6C1B1B),
+                background: iced::color!(0x2C323A),
+                text: iced::color!(0xCAC2B6),
+                primary: iced::color!(0xC79F60),
+                success: iced::color!(0x659B5E), // NOT USED FOR NOW
+                danger: iced::color!(0x6C1B1B), // NOT USED FOR NOW
             })
         }
     }
