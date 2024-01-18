fb2905059603627fb504c745c62ca252aaa9ee2b
Exidex <16986685+Exidex@users.noreply.github.com>
2024-01-18 20:21:07 +0100

    Remove deno references in lib.deno.d.ts

--- js/deno/generator/index.ts
+++ js/deno/generator/index.ts
@@ -6,10 +6,11 @@ const LIB_DENO_DECLARATION_URL = "https://github.com/denoland/deno/releases/down
 const res = await fetch(LIB_DENO_DECLARATION_URL);
 const content = await res.text();
 
+const fixedContent = content.replaceAll(/\/\/\/ <reference lib="deno\..*" \/>/g, "")
 
 const distDir = "./dist";
 if (!existsSync(distDir)) {
     mkdirSync(distDir);
 }
 
-writeFileSync(`${distDir}/lib.deno.d.ts`, content)
+writeFileSync(`${distDir}/lib.deno.d.ts`, fixedContent)
