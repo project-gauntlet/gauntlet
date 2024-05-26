48cf2d985f75709c1fe980debb1b828acc1bf829
Exidex <16986685+Exidex@users.noreply.github.com>
2024-05-26 21:31:11 +0200

    Serialize byte array as base64 in scenarios

--- rust/common/src/scenario_model.rs
+++ rust/common/src/scenario_model.rs
@@ -21,6 +21,7 @@ pub enum ScenarioUiPropertyValue {
     String(String),
     Number(f64),
     Bool(bool),
+    #[serde(with="base64")]
     Bytes(Vec<u8>),
     Object(HashMap<String, ScenarioUiPropertyValue>),
     Undefined,
@@ -44,4 +45,21 @@ pub enum ScenarioFrontendEvent {
         entrypoint_id: String,
         render_location: ScenarioUiRenderLocation,
     },
+}
+
+mod base64 {
+    use serde::{Serialize, Deserialize};
+    use serde::{Deserializer, Serializer};
+    use base64::Engine;
+
+    pub fn serialize<S: Serializer>(v: &Vec<u8>, s: S) -> Result<S::Ok, S::Error> {
+        let base64 = base64::engine::general_purpose::STANDARD.encode(v);
+        String::serialize(&base64, s)
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error> {
+        let base64 = String::deserialize(d)?;
+        base64::engine::general_purpose::STANDARD.decode(base64.as_bytes())
+            .map_err(|e| serde::de::Error::custom(e))
+    }
 }
