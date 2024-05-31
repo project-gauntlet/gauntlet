03f7ea1c72514806112708a6502efcf7eb8d2fa8
Exidex <16986685+Exidex@users.noreply.github.com>
2024-05-31 15:35:45 +0200

    Tweak detail content code block scenario

--- scenarios/plugins/docs_detail/src/content_code_block.tsx
+++ scenarios/plugins/docs_detail/src/content_code_block.tsx
@@ -1,12 +1,20 @@
 import { Detail } from "@project-gauntlet/api/components";
 import { ReactNode } from "react";
 
+
+const code = `
+    fib :: Integer -> Integer
+    fib 0 = 0
+    fib 1 = 1
+    fib n = fib (n-1) + fib (n-2)
+`.trimStart().replace(/^ {4}/g, '');
+
 export default function Main(): ReactNode {
     return (
         <Detail>
             <Detail.Content>
                 <Detail.Content.CodeBlock>
-                    "Bring the Dominion of the Ezaraa across the stars! And consume the flesh of all the lesser species!"
+                    {code}
                 </Detail.Content.CodeBlock>
             </Detail.Content>
         </Detail>
