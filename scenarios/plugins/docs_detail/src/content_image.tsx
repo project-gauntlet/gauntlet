d13c3192f647cdcd7e1a22bea43e58ffd5e11217
Exidex <16986685+Exidex@users.noreply.github.com>
2024-05-31 15:34:16 +0200

    Fix content image scenario

--- scenarios/plugins/docs_detail/src/content_image.tsx
+++ scenarios/plugins/docs_detail/src/content_image.tsx
@@ -1,14 +1,19 @@
 import { Detail } from "@project-gauntlet/api/components";
 import { ReactNode } from "react";
 
+async function readFile(url: string): Promise<ArrayBuffer> {
+    const res = await fetch(url);
+    const blob = await res.blob();
+    return await blob.arrayBuffer()
+}
+
+const img = await readFile("https://static.wikia.nocookie.net/starwars/images/a/ae/The_Whills_Strike_Back.png/revision/latest/scale-to-width-down/400?cb=20201006180053")
+
 export default function Main(): ReactNode {
     return (
         <Detail>
             <Detail.Content>
-                <Detail.Content.CodeBlock>
-                    TODO
-                    &lt;Detail.Content.Image source=&#123;&#123; data: ... &#125;&#125;/&gt;
-                </Detail.Content.CodeBlock>
+                <Detail.Content.Image source={{ data: img }}/>
             </Detail.Content>
         </Detail>
     )
