c3c58d31283207bd5918edd67ad188f21989a193
Exidex <16986685+Exidex@users.noreply.github.com>
2024-10-18 19:10:30 +0200

    Reduce scope of try catch in calculator bundled plugin

--- bundled_plugins/gauntlet/src/calculator.tsx
+++ bundled_plugins/gauntlet/src/calculator.tsx
@@ -13,45 +13,51 @@ interface InternalApi {
 export default function Calculator(props: { text: string }): ReactNode | undefined {
     const text = props.text;
 
-    try {
-        if (text.length < 3) {
-            return undefined
-        }
-
-        const { left, right } = InternalApi.run_numbat(text);
-
-        if (left == right) {
-            return undefined
-        }
-
-        return (
-            <Inline
-                actions={
-                    <ActionPanel>
-                        <Action
-                            label={"Copy result"}
-                            onAction={async () => {
-                                await Clipboard.writeText(right)
-                                showHud("Result copied")
-                            }}
-                        />
-                    </ActionPanel>
-                }
-            >
-                <Inline.Left>
-                    <Content.H3>
-                        {left}
-                    </Content.H3>
-                </Inline.Left>
-                <Inline.Separator icon={Icons.ArrowRight}/>
-                <Inline.Right>
-                    <Content.H3>
-                        {right}
-                    </Content.H3>
-                </Inline.Right>
-            </Inline>
-        )
-    } catch (e) {
+    if (text.length < 3) {
         return undefined
     }
+
+    let result;
+
+    try {
+         result = InternalApi.run_numbat(text);
+    } catch (e) {
+        // this view is executed on every key press in main search bar
+        // when numbat run fails it means expression is not valid so we return here and do not show inline view
+        return undefined
+    }
+
+    const { left, right } = result;
+
+    if (left == right) {
+        return undefined
+    }
+
+    return (
+        <Inline
+            actions={
+                <ActionPanel>
+                    <Action
+                        label={"Copy result"}
+                        onAction={async () => {
+                            await Clipboard.writeText(right)
+                            showHud("Result copied")
+                        }}
+                    />
+                </ActionPanel>
+            }
+        >
+            <Inline.Left>
+                <Content.H3>
+                    {left}
+                </Content.H3>
+            </Inline.Left>
+            <Inline.Separator icon={Icons.ArrowRight}/>
+            <Inline.Right>
+                <Content.H3>
+                    {right}
+                </Content.H3>
+            </Inline.Right>
+        </Inline>
+    )
 }
