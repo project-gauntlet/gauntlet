479594b38500e073c6a366df559e0a0c3df6fd6d
Exidex <16986685+Exidex@users.noreply.github.com>
2024-11-10 17:48:15 +0100

    Fix command generator example in dev plugin

--- dev_plugin/src/command-generator.ts
+++ dev_plugin/src/command-generator.ts
@@ -1,60 +1,57 @@
-import { GeneratedCommand, showHud } from "@project-gauntlet/api/helpers";
+import { GeneratorProps, showHud } from "@project-gauntlet/api/helpers";
 
-export default function CommandGenerator(): GeneratedCommand[] {
-    return [
-        {
-            id: 'generated-test-1',
-            name: 'Generated Item 1',
-            fn: () => {
-                new Promise(() => {
-                    throw new Error("gen")
-                })
+export default function CommandGenerator({ add, remove: _ }: GeneratorProps): void {
+    add('generated-test-1', {
+        name: 'Generated Item 1',
+        fn: () => {
+            new Promise(() => {
+                throw new Error("gen")
+            })
 
-                console.log('generated-test-1')
-            }
-        },
-        {
-            id: 'generated-test-2',
-            name: 'Generated Item 2',
-            fn: () => {
-                console.log('generated-test-2')
-
-                sessionStorage.setItem("test", "test")
-                console.dir(sessionStorage.getItem("test"))
-
-                localStorage.setItem("test", "test")
-                console.dir(localStorage.getItem("test"))
-            },
-            actions: [
-                {
-                    label: "Test 1",
-                    fn: () => {
-                        console.log('generated-action-1')
-                    }
-                },
-                {
-                    ref: "testGeneratedAction1",
-                    label: "Test 2",
-                    fn: () => {
-                        console.log('generated-action-2')
-                    }
-                }
-            ]
-        },
-        {
-            id: 'generated-test-3',
-            name: 'Generated Item 3',
-            fn: () => {
-                showHud("HUD test display")
-                console.log('generated-test-3')
-            }
-        },
-        {
-            id: 'generated-test-4',
-            name: 'Generated Item 4',
-            fn: () => {
-                console.log('generated-test-4')
-            }
+            console.log('generated-test-1')
         }
-    ]
+    })
+
+    add('generated-test-2', {
+        name: 'Generated Item 2',
+        fn: () => {
+            console.log('generated-test-2')
+
+            sessionStorage.setItem("test", "test")
+            console.dir(sessionStorage.getItem("test"))
+
+            localStorage.setItem("test", "test")
+            console.dir(localStorage.getItem("test"))
+        },
+        actions: [
+            {
+                label: "Test 1",
+                fn: () => {
+                    console.log('generated-action-1')
+                }
+            },
+            {
+                ref: "testGeneratedAction1",
+                label: "Test 2",
+                fn: () => {
+                    console.log('generated-action-2')
+                }
+            }
+        ]
+    })
+
+    add('generated-test-3', {
+        name: 'Generated Item 3',
+        fn: () => {
+            showHud("HUD test display")
+            console.log('generated-test-3')
+        }
+    })
+
+    add('generated-test-4', {
+        name: 'Generated Item 4',
+        fn: () => {
+            console.log('generated-test-4')
+        }
+    })
 }
