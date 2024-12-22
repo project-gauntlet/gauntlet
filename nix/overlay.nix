95b0243d5b2fb8bae0de44d7c39712f77cca1860
Exidex <16986685+exidex@users.noreply.github.com>
2024-12-22 13:18:10 +0100

    Bump version in nix overlay

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -5,7 +5,7 @@
 }: {
   flake.overlays.default = final: _: let
     # These must be updated following the instructions in ./nix/README.md when dependencies are updated or the version bumped
-    version = "v11";
+    version = "v12";
     npmDepsHash = "sha256-zcqn+EsxWfdxkIKUnF8kiuURtPiE6XQOaDAspuKEe64=";
     RUSTY_V8_ARCHIVE = fetchRustyV8 "130.0.2" {
       aarch64-darwin = "sha256-aWZ/4Q4Wttx37xOdBmTCPGP+eYGhr4CM1UkYq8pC7Qs=";
