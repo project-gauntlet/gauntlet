e88d0d7b02a8ae001ce5dcd8f4fda551feade0b8
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-03-07 00:24:30 +0100

    rename filename

--- rust/manifest_schema/src/main.rs
+++ rust/manifest_schema/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let schema = schema_for!(PluginManifest);
     let json = serde_json::to_string_pretty(&schema).unwrap();
 
-    let mut file = File::create("schema.json").expect("Failed to create schema.json");
+    let mut file = File::create("plugin_manifest.schema.json").expect("Failed to create schema.json");
     file.write_all(json.as_bytes()).expect("Failed to write schema");
 
     println!("Schema generated and saved to schema.json");
