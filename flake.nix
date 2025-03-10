d4e88b97a59a3770dd5fbbb99b7715313b17aae3
DaRacci <me@racci.dev>
2025-03-10 06:31:09 +0000

    fix(flake): use strings instead of deprecated url literals.

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "https://github.com/project-gauntlet/gauntlet";
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} {imports = [./nix];};
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    systems.url = github:nix-systems/default;
-    flake-parts.url = github:hercules-ci/flake-parts;
-    flake-compat.url = github:edolstra/flake-compat;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
-    crane.url = github:ipetkov/crane;
+    crane.url = "github:ipetkov/crane";
   };
 }
