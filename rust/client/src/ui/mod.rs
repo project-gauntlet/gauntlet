d6515f35fe8239a3ecb970041403d3eca2b63af0
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-03-23 08:51:33 +0100

    fix: ensure window state resets correctly
    instead of reseting window state in `show_window` reset it in the
    `hide_window` process

--- rust/client/src/ui/mod.rs
+++ rust/client/src/ui/mod.rs
@@ -2315,6 +2315,10 @@ impl AppModel {
             GlobalState::PendingPluginView { .. } => {}
         }
 
+        if self.pending_window_state_reset {
+            commands.push(self.reset_window_state());
+        }
+
         Task::batch(commands)
     }
 
@@ -2347,11 +2351,11 @@ impl AppModel {
             window::change_mode(self.main_window_id, Mode::Windowed),
         ]);
 
-        let mut commands = vec![open_task];
+        let commands = vec![open_task];
 
-        if self.pending_window_state_reset {
-            commands.push(self.reset_window_state());
-        }
+        // if self.pending_window_state_reset {
+        //     commands.push(self.reset_window_state());
+        // }
 
         Task::batch(commands)
     }
